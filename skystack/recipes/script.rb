1b4ac22fd009a93772ab818e37fae940d07c7262
Tass Skoudros <support@skystack.com>
2011-07-17 12:00:26 +0100

    modified skystack::scripts recipe to depend less on meta info from the api

--- skystack/recipes/script.rb
+++ skystack/recipes/script.rb
@@ -17,30 +17,43 @@
 #
 
 =beginjson
-{"name":"skystack::script","methods":["run_script","edit_script","delete_script"],"symbol":":script","properties":{"skyscript_id":"string","resource":"string"}}
+{"name":"skystack::script","methods":["run_script","edit_script","delete_script"],"symbol":":scripts","properties":{":skyscript_id":"string",":resource":"string"}}
 =end
 
 script = node.run_state[:current_app]
 
-execute "run_skyscript_#{script[:skyscript_id]}" do
-  command "bash /tmp/#{script[:skyscript_id]}.sh;touch /opt/skystack/bootstrapper/executed-#{script[:skyscript_id]}"
-  action :nothing
-  only_if do ! File.exists?( "/opt/skystack/bootstrapper/executed-#{script[:skyscript_id]}" ) end
-end
-#all interactions with our API should be done via a Ruby script to cleanup the execution of a SkyScript.
-bash "get_skyscript" do
-  interpreter "#{script[:type]}"
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-  export SKYSTACK_PATH=/opt/skystack
-  export SS_ALIAS=`awk '/SS_ALIAS/ {print $2}' FS=\= $SKYSTACK_PATH/bootstrapper/etc/userdata.conf` > /dev/null 2>&1
-  export SS_APITOKEN=`awk '/SS_APITOKEN/ {print $2}' FS=\= $SKYSTACK_PATH/bootstrapper/etc/userdata.conf` > /dev/null 2>&1
-  export SS_APIUSER=`awk '/SS_APIUSER/ {print $2}' FS=\= $SKYSTACK_PATH/bootstrapper/etc/userdata.conf` > /dev/null 2>&1
+Chef::Log.info "skystack::script telling chef to run this script #{script[:skyscript_id]}"
+node[:scripts].each do |script|
+  script[:ext] = File.extname(script[:resource])
+  execute "run_skyscript_#{script[:skyscript_id]}" do
+    command "/tmp/#{script[:skyscript_id]}#{script[:ext]};touch /opt/skystack/tmp/executed-#{script[:skyscript_id]}"
+    action :nothing
+    only_if do ! File.exists?( "/opt/skystack/tmp/executed-#{script[:skyscript_id]}" ) end
+  end
+  #all interactions with our API should be done via a Ruby script to cleanup the execution of a SkyScript.
+  Chef::Log.info "skystack::script via skystack we fetch the script contents #{script[:skyscript_id]}"
+  bash "get_skyscript" do
+    interpreter "#{ext}"
+    user "root"
+    cwd "/tmp"
+    code <<-EOH
+    export SKYSTACK_PATH=/opt/skystack
+    export SS_ALIAS=`awk '/SS_ALIAS/ {print $2}' FS=\= $SKYSTACK_PATH/etc/userdata.conf` > /dev/null 2>&1
+    export SS_APITOKEN=`awk '/SS_APITOKEN/ {print $2}' FS=\= $SKYSTACK_PATH/etc/userdata.conf` > /dev/null 2>&1
+    export SS_APIUSER=`awk '/SS_APIUSER/ {print $2}' FS=\= $SKYSTACK_PATH/etc/userdata.conf` > /dev/null 2>&1
+    export SS_BASE=`awk '/SS_BASE/ {print $2}' FS=\= $SKYSTACK_PATH/etc/userdata.conf` > /dev/null 2>&1 
 
-  curl -o /tmp/tmp_#{script[:skyscript_id]}.sh -u $SS_APIUSER:$SS_APITOKEN https://my.skystack.com/$SS_ALIAS/sky_build/sky_scripts/#{script[:skyscript_id]}.bash
-  tr -d '\015\032' < /tmp/tmp_#{script[:skyscript_id]}.#{script[:ext]} > /tmp/#{script[:skyscript_id]}.#{script[:ext]}
-  EOH
-  notifies :run, resources(:execute => "run_skyscript_#{script[:skyscript_id]}")
-  creates "/tmp/tmp_#{script[:skyscript_id]}.#{script[:ext]}"
+    if [ $SS_BASE = my.skystack.local ];then
+    	HTTP=http
+    else
+    	HTTP=https
+    fi
+  
+    curl -o /tmp/#{script[:skyscript_id]}_part -u $SS_APIUSER:$SS_APITOKEN $HTTP://$SS_BASE/$SS_ALIAS/scripts/#{script[:skyscript_id]}#{script[:ext]}
+    tr -d '\015\032' < /tmp/#{script[:skyscript_id]}_part > /tmp/#{script[:skyscript_id]}#{script[:ext]}
+    chmod +x /tmp/#{script[:skyscript_id]}#{script[:ext]}
+    EOH
+    notifies :run, resources(:execute => "run_skyscript_#{script[:skyscript_id]}")
+    creates "/tmp/#{script[:skyscript_id]}#{script[:ext]}"
+  end
 end
