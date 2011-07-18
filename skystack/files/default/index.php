d8dda230df70303f0678e6444f54767ce13a641c
Tass Skoudros <support@skystack.com>
2011-07-18 07:34:36 +0100

    mysql constant error, probably the gem not being installed at the right time

--- skystack/files/default/index.php
+++ skystack/files/default/index.php
@@ -2,10 +2,10 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>On-Demand Cloud Management - Skystack&reg;	</title>
+	<title>Made by Skystack</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
-<link rel="stylesheet" type="text/css" href="http://my.skystack.com/css/base.css" />
-<link rel="stylesheet" type="text/css" href="http://my.skystack.com/css/redmond/jquery-ui-1.7.1.custom.css" />
+<link rel="stylesheet" type="text/css" href="http://my.skystack.local/css/base.css" />
+<link rel="stylesheet" type="text/css" href="http://my.skystack.local/css/redmond/jquery-ui-1.7.1.custom.css" />
 
 </head>
 <body>
